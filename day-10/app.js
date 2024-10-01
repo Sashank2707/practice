21f50475b7f0094be3a6668e0c7e4b230be6ed2a
unknown <snehaakuthota5@gmail.com>
2024-10-01 03:52:00 -0700

    Practice Work

---
+++ day-10/app.js
@@ -0,0 +1,29 @@
+/*let btn1 = document.createElement("button");
+btn1.innerHTML="click";
+btn1.addEventListener("click",myFun);
+
+let h1 = document.getElementById("heading");
+h1.appendChild(btn1);
+function myFun(){
+    document.write("my name is Sneha");
+}
+let btn2 = document.createElement("button");
+btn2.innerHTML="Hide";
+btn2.addEventListener("click",myfun2);
+ document.getElementById("heading");
+ h1.appendChild(btn2);
+ function myFun2(){
+    document.write(".....");
+ }*/
+
+   let b1= document.getElementById('btn1');
+   let b2= document.getElementById('btn2');
+   let h1= document.getElementById('heading');
+   function show(){
+    h1.textcontent ="My name is sneha";
+   }
+   function hide(){
+    h1.textcontent="....";
+   }
+
+
---
+++ work/app.js
@@ -0,0 +1,43 @@
+// let b1 = document.createElement("button");
+// b1.innerHTML='Show!'
+// let b2 = document.createElement("button");
+// b2.innerHTML='Hide!'
+// let b3 = document.createElement("button");
+// b3.innerHTML="changeBg"
+// let b4 = document.createElement("button");
+// b4.innerHTML="changecolor!"
+// let h = document.createElement("h1");
+// h.innerHTML="...."
+
+
+// let mainDiv=document.getElementById("main");
+// mainDiv.append(b1,b2,h,b3,b4);
+
+// b1.addEventListener("click",show);
+// b2.addEventListener("click",hide);
+// b3.addEventListener("click",change);
+// b4.addEventListener("click",color);
+
+
+// function show(){
+//     h.textContent="my name is sneha";
+    
+
+// }
+// function hide(){
+//     h.textContent="...";
+// }
+// function change(){
+//     h.style.backgroundColor="pink";
+// }
+// function color(){
+//     h.style.color="chocolate";
+// }
+
+
+let object = {
+    "username":"sneha",
+    "age":"19",
+    "role":"developer"
+}
+console.log(object);
